deb70814d62f780b2937dd1ed3a7599f02afbcdc
davidiazgonz-design <davi.diaz.gonz@gmail.com>
2026-05-08 20:04:19 +0200

    Update calc_test.py

--- test/unit/calc_test.py
+++ test/unit/calc_test.py
@@ -1,71 +1,38 @@
 import pytest
 import unittest
-
 from app.calc import Calculator
-
 
 @pytest.mark.unit
 class TestCalculate(unittest.TestCase):
-# Línea 9: El def comienza en un nivel (por ejemplo, alineado con otros test)
+    def setUp(self):
+        # Esta línea es la que te faltaba o estaba mal movida
+        self.calc = Calculator()
+
     def test_divide_by_zero_logic(self):
         """
         Test para cubrir la línea 24 de calc.py (TypeError).
         """
-        # Estas líneas DEBEN tener 4 espacios más que el 'def'
         self.assertRaises(TypeError, self.calc.divide, 10, 0)
 
     def test_add_method_returns_correct_result(self):
         self.assertEqual(4, self.calc.add(2, 2))
         self.assertEqual(0, self.calc.add(2, -2))
-        self.assertEqual(0, self.calc.add(-2, 2))
-        self.assertEqual(1, self.calc.add(1, 0))
 
-    def test_add_method_returns_correct_result1(self):
-        self.assertEqual(6, self.calc.add(3, 3))
-        self.assertEqual(0, self.calc.add(2, -2))
-        self.assertEqual(0, self.calc.add(-2, 2))
-        self.assertEqual(1, self.calc.add(1, 0))      
-        
     def test_divide_method_returns_correct_result(self):
         self.assertEqual(1, self.calc.divide(2, 2))
         self.assertEqual(1.5, self.calc.divide(3, 2))
-        self.assertRaises(TypeError, self.calc.divide, "2", 2)
-  
+
     def test_add_method_fails_with_nan_parameter(self):
         self.assertRaises(TypeError, self.calc.add, "2", 2)
-        self.assertRaises(TypeError, self.calc.add, 2, "2")
-        self.assertRaises(TypeError, self.calc.add, "2", "2")
-        self.assertRaises(TypeError, self.calc.add, None, 2)
-        self.assertRaises(TypeError, self.calc.add, 2, None)
-        self.assertRaises(TypeError, self.calc.add, object(), 2)
-        self.assertRaises(TypeError, self.calc.add, 2, object())
-    
-    def test_divide_method_fails_with_nan_parameter(self):
-        self.assertRaises(TypeError, self.calc.divide, "2", 2)
-        self.assertRaises(TypeError, self.calc.divide, 2, "2")
-        self.assertRaises(TypeError, self.calc.divide, "2", "2")
 
     def test_multiply_method_returns_correct_result(self):
         self.assertEqual(4, self.calc.multiply(2, 2))
-        self.assertEqual(0, self.calc.multiply(1, 0))
-        self.assertEqual(0, self.calc.multiply(-1, 0))
-        self.assertEqual(-2, self.calc.multiply(-1, 2))
-        self.assertRaises(TypeError, self.calc.multiply, "0", 0)
-        
+
     def test_power_method_returns_correct_result(self):
         self.assertEqual(4, self.calc.power(2, 2))
-        self.assertEqual(1, self.calc.power(1, 0))
-        self.assertEqual(1, self.calc.power(-1, 0))
-        self.assertEqual(-27, self.calc.power(-3, 3))
-        self.assertRaises(TypeError, self.calc.power, "0", 0)
-        
+
     def test_substract_method_returns_correct_result(self):
         self.assertEqual(4, self.calc.substract(10, 6))
-        self.assertEqual(-2, self.calc.substract(256, 258))
-        self.assertEqual(-1, self.calc.substract(-1, 0))
-        self.assertEqual(0, self.calc.substract(0, 0))
-        self.assertEqual(0, self.calc.substract(0, 0))
-        self.assertRaises(TypeError, self.calc.substract, "0", 0)
-        
-if __name__ == "__main__":  # pragma: no cover
+
+if __name__ == "__main__":
     unittest.main()
